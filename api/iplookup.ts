996d81410014a44bdb68612f2b2193df8164b107
cherryhikari <cherryhikari@outlook.com>
2025-10-19 19:34:33 +0800

    impl error try catch

--- api/iplookup.ts
+++ api/iplookup.ts
@@ -2,9 +2,13 @@ import type { VercelRequest, VercelResponse } from '@vercel/node'
 import axios from 'axios'
 
 export default async (req: VercelRequest, res: VercelResponse) => {
-  const re = await axios.get('https://api.ipify.org?format=json')
+  try {
+    const re = await axios.get('https://api.ipify.org?format=json')
   return res.status(re.status).json({
     result:re.statusText,
     data:re.data
   })
+  } catch (error) {
+    return res.status(500).json({ error: 'Failed to fetch IP information.', data: error });
+  }
 }
